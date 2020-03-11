48cc45022bc2c79773288b63a90eecf444afb97c
TimBik <timurbikmullin@mail.ru>
2020-03-12 00:21:19 +0300

    add project

---
+++ src/main/java/ru/itis/javalab/servlets/RegistrationServlet.java
@@ -0,0 +1,45 @@
+package ru.itis.javalab.servlets;
+
+import org.springframework.context.ApplicationContext;
+import ru.itis.javalab.dto.SignUpDto;
+import ru.itis.javalab.dto.UserDto;
+import ru.itis.javalab.services.ParametrLoader;
+import ru.itis.javalab.services.RegistrationService;
+import ru.itis.javalab.services.ViewResolver;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+
+@WebServlet("/registration")
+public class RegistrationServlet extends HttpServlet {
+    private ViewResolver viewResolver;
+    private ParametrLoader parameterLoader;
+    private RegistrationService registrationService;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        viewResolver.process("registration.ftl", new HashMap<>(), resp.getWriter());
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse resp) {
+        SignUpDto signUpData = parameterLoader.getRegistrationParams(request);
+        registrationService.loadUserFromParameters(signUpData);
+    }
+
+    @Override
+    public void init(ServletConfig config) {
+        ServletContext context = config.getServletContext();
+        ApplicationContext springContext = (ApplicationContext) context.getAttribute("springContext");
+        viewResolver = springContext.getBean(ViewResolver.class);
+        parameterLoader = springContext.getBean(ParametrLoader.class);
+        registrationService = springContext.getBean(RegistrationService.class);
+    }
+
+}
